7c56fa31a29e241e98d4e6f52b9cbbd070be2795
kazuo takagi <takagi@tranq.jp>
2017-06-09 20:21:37 +0900

    Dockerfile's bug and intelmq's problem

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.4
 
 RUN apt-get update -y 
 RUN apt-get upgrade -y 
-RUN apt-get install -y git build-essential libcurl4-gnutls-dev libgnutls28-dev python3-dev apache2 php5 libapache2-mod-php5 supervisor && \
+RUN apt-get install -y git build-essential libcurl4-gnutls-dev libgnutls28-dev python3-dev apache2 php5 libapache2-mod-php5 supervisor redis-server sudo && \
 	apt-get clean && \
 	rm -rf /var/lib/apt/lists/ /tmp/ /var/tmp/*
 	
@@ -14,16 +14,23 @@ RUN mv /opt/intelmq/etc/examples/*.conf /opt/intelmq/etc/
 COPY config/* /opt/intelmq/etc/
 
 RUN useradd -d /opt/intelmq -U -s /bin/bash intelmq && \
+    mkdir -p /opt/intelmq/var/log && \
+    touch /opt/intelmq/var/log/intelmqctl.log && \
     chmod -R 0770 /opt/intelmq && \
+    chown intelmq.intelmq /opt/intelmq/var/log/intelmqctl.log && \
     chown -R intelmq.intelmq /opt/intelmq
 
 RUN   git clone https://github.com/certtools/intelmq-manager.git /tmp/intelmq-manager && \
     cp -R /tmp/intelmq-manager/intelmq-manager/* /var/www/html/ && \
     chown -R www-data.www-data /var/www/html/ && \
-    usermod -a -G intelmq www-data && \
-    echo "www-data ALL=(intelmq) NOPASSWD: /usr/local/bin/intelmqctl" >> /etc/sudoers
+    usermod -a -G intelmq www-data
+   
+RUN echo "www-data ALL=(intelmq) NOPASSWD: /usr/local/bin/intelmqctl" > /etc/sudoers.d/intelmq && \
+    chmod 0440 /etc/sudoers.d/intelmq
 
+COPY intelmq_redis.conf /etc/redis/intelmq_redis.conf
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+COPY pipeline.js /var/www/html/js/pipeline.js
 
 EXPOSE 80
 EXPOSE 443
---
+++ pipeline.js
@@ -0,0 +1,71 @@
+function generate_pipeline_conf(edges) {
+    var conf_string = '';
+    var new_edges = {};
+    
+    for (index in edges) {
+        var edge = edges[index];
+        
+        if (!new_edges[edge.from]) {
+            new_edges[edge.from] = {
+                'source-queue': [],
+                'destination-queues': []
+            }
+        }
+        
+        if (!new_edges[edge.to]) {
+            new_edges[edge.to] = {
+                'source-queue': [],
+                'destination-queues': []
+            }
+        }
+        
+        new_edges[edge.from]['destination-queues'].push(edge.to + '-queue');
+        new_edges[edge.to]['source-queue'].push(edge.from + '-queue');
+    }
+    
+    for (id in new_edges) {
+        var edge = new_edges[id];
+        
+        if (edge['source-queue'].length > 0) {
+            edge['source-queue'] = id + '-queue';
+        } else {
+            edge['source-queue'] = undefined;
+        }
+        
+        if (edge['destination-queues'].length == 0) {
+            edge['destination-queues'] = undefined;
+        } else {
+            edge['destination-queues'].sort()
+        }
+        edge = sortObjectByPropertyName(edge)
+    }
+
+    new_edges = sortObjectByPropertyName(new_edges)
+    return JSON.stringify(new_edges, undefined, 4);
+}
+
+function read_pipeline_conf(config, nodes) {
+    var edges = {};
+    var i = 0;
+    
+    for (from in config) {
+        if (config[from]['destination-queues'] != undefined) {
+            for (index in config[from]['destination-queues']) {
+                var to_node = config[from]['destination-queues'][index].replace(/-queue$/, "");
+                if(nodes[from] != undefined && nodes[to_node] != undefined) {
+                    var edge_id = 'edge' + i++;
+                    var new_edge = {
+                        'id': edge_id,
+                        'from': from,
+                        'to': to_node
+                    };
+                    
+                    edges[edge_id]=new_edge;
+                }
+            }
+        }
+    }
+    
+    return edges;
+}
+
